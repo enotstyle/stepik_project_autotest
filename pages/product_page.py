a3309f605ad928a22e29c2ced3f9f7a307e8cddb
Ivan <vanek-te@mail.ru>
2022-07-14 09:57:20 +0700

    fix lesson 4.3 step 2

--- pages/product_page.py
+++ pages/product_page.py
@@ -1,8 +1,18 @@
 from .base_page import BasePage
 from .locators import ProductPageLocators
+import time
 
 class ProductPage(BasePage):
     def add_to_basket(self):
         basket_button = self.browser.find_element(*ProductPageLocators.BASKET_BUTTON)
         basket_button.click()
 
+    def should_be_message_about_adding(self):
+        book_name = self.browser.find_element(*ProductPageLocators.BOOK_NAME).text
+        add_message = self.browser.find_element(*ProductPageLocators.ADD_MESSAGE).text
+        assert book_name in add_message, 'Not book name in the message'
+
+    def should_be_message_basket_total(self):
+        book_price = self.browser.find_element(*ProductPageLocators.BOOK_PRICE).text
+        price_message = self.browser.find_element(*ProductPageLocators.PRICE_MESSAGE).text
+        assert book_price == price_message, 'Book prise != price in the message'
--- test_product_page.py
+++ test_product_page.py
@@ -1,4 +1,5 @@
 from .pages.product_page import ProductPage
+import time
 
 def test_guest_can_add_product_to_basket(browser):
     link = "http://selenium1py.pythonanywhere.com/catalogue/the-shellcoders-handbook_209/?promo=newYear"
@@ -6,3 +7,5 @@ def test_guest_can_add_product_to_basket(browser):
     page.open()
     page.add_to_basket()
     page.solve_quiz_and_get_code()
+    page.should_be_message_about_adding()
+    page.should_be_message_basket_total()
